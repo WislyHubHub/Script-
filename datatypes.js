c27d05ce322e249efccd51c41b2a983b0d3ef756
WislyHubHub <rachapruek12345@gmail.com>
2022-12-21 09:26:04 +0700

    updete

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+const people = ["Aaron" , "Mel" , "Jhon"]
+const one = 1 ;
+const str = "Hello world" ;
+const b = true ;
+const employee = {
+    firstName : "Rachapruek" ,
+    lastName : "Ariyapruek",
+
+} ;
+
+function sayHello (person) {
+    console.log("Hello " + person.firstName);
+
+}
+
+console.log (typeof people) ;
+console.log (typeof sayHello)
+console.log (empleyee instanceof Array) ;
+sayHello(employee) ;
